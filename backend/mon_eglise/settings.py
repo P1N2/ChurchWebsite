42ab187344078a7674b8782aaf4acb00401463c2
Penouel aoga <penouelaoga@gmail.com>
2025-09-04 23:42:32 +0100

    modified settings

--- backend/mon_eglise/settings.py
+++ backend/mon_eglise/settings.py
@@ -80,16 +80,17 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 import os
+import dj_database_url
+DATABASE_URL = os.environ.get('MYSQL_PUBLIC_URL', 'mysql://root:fLLGHuhjoFiGeooRXdusaYbAGMMNFwWJ@mainline.proxy.rlwy.net:37910/railway')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
-        'USER': os.environ.get('MYSQL_USER'),
-        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
-        'HOST': os.environ.get('MYSQL_HOST'),
-        'PORT': os.environ.get('MYSQL_PORT', 3306),
-    }
+    'default': dj_database_url.parse(DATABASE_URL)
 }
+# DATABASES = {
+#     'default': {
+#         'NAME': os.environ.get('mysql://root:fLLGHuhjoFiGeooRXdusaYbAGMMNFwWJ@mainline.proxy.rlwy.net:37910/railway'),
+#     }
+# }
 
 
 
