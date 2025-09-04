874480967ebbd702c7393237591fadd3bc9b7eed
Penouel aoga <penouelaoga@gmail.com>
2025-09-04 22:03:26 +0100

    api

--- backend/mon_eglise/urls.py
+++ backend/mon_eglise/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('church.urls')),  # Ajoute ça pour l’API
+    path('api/', include('church.urls')),  # Change this line
 ]
 
 if settings.DEBUG:
